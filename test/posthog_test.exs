29cd90ba8eda649d45fec8b6c2d2b363089eb118
Rafa Audibert <rafaeelaudibert@gmail.com>
2026-02-05 00:55:34 -0300

    refactor: mix format

--- test/posthog_test.exs
+++ test/posthog_test.exs
@@ -140,7 +140,11 @@ defmodule PostHogTest do
       assert [event] = all_captured()
 
       assert is_binary(event.uuid)
-      assert Regex.match?(~r/^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i, event.uuid)
+
+      assert Regex.match?(
+               ~r/^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i,
+               event.uuid
+             )
     end
   end
 
