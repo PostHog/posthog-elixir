decd07eed2117806eed1f21d19ae0c2b7dbd46c6
Phil Haack <phil.h@posthog.com>
2026-06-10 09:21:55 -0700

    style: alias PostHog.API.Client in client test

--- test/posthog/api/client_test.exs
+++ test/posthog/api/client_test.exs
@@ -1,10 +1,11 @@
 defmodule PostHog.API.ClientTest do
   use ExUnit.Case, async: true
 
-  test "client/2 sets the posthog-elixir User-Agent" do
-    %PostHog.API.Client{client: req} =
-      PostHog.API.Client.client("phc_test", "https://us.i.posthog.com")
+  alias PostHog.API.Client
 
-    assert req.headers["user-agent"] == [PostHog.API.Client.user_agent()]
+  test "client/2 sets the posthog-elixir User-Agent" do
+    %Client{client: req} = Client.client("phc_test", "https://us.i.posthog.com")
+
+    assert req.headers["user-agent"] == [Client.user_agent()]
   end
 end
